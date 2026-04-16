e355195bcc763a9e0a8bd71167eb1f10c825b2f4
omersusin <tekgercek73@gmail.com>
2026-04-16 15:03:20 +0300

    Add Hilt plugin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
-    id("com.google.dagger.hilt.android")
+    id("com.google.dagger.hilt.android") version "2.50"
 }
 
 android {
