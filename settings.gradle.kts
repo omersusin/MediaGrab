776c5dd701e3f6573646a90d977a3cf0bcfcaa1c
omersusin <tekgercek73@gmail.com>
2026-06-16 21:53:47 +0300

    fix: remove hilt resolutionStrategy workaround

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,13 +11,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.google.dagger.hilt.android") {
-                useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
-            }
-        }
-    }
 }
 
 dependencyResolutionManagement {
