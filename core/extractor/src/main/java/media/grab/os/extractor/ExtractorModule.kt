fa3f7e50e59aa9b4e872e846f553b2c173f679d0
omersusin <tekgercek73@gmail.com>
2026-06-20 15:40:17 +0300

    fix: remove duplicate old extractor placeholders

--- core/extractor/src/main/java/media/grab/os/extractor/ExtractorModule.kt
+++
@@ -1,4 +0,0 @@
-package media.grab.os.extractor
-
-// Stub - Hilt devre dışı
-object ExtractorModule
--- core/extractor/src/main/java/media/grab/os/extractor/GenericExtractor.kt
+++
@@ -1,9 +0,0 @@
-package media.grab.os.extractor
-
-class GenericExtractor : Extractor {
-    override val name = "Generic"
-    override val supportedHosts = listOf("instagram.com", "tiktok.com", "twitter.com", "x.com")
-    override suspend fun extract(url: String): ExtractionResult {
-        return ExtractionResult(success = false, error = "Generic extractor not implemented")
-    }
-}
